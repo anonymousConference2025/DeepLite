58d8b1820f1ae83b3991c327ea9244951452261e
anonymousConference2025 <164576649+anonymousConference2025@users.noreply.github.com>
2024-03-23 04:56:05 -0400

    EMNIST

---
+++ DeepReduce Code/EMNIST/M1/excuate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Run Python files one by one
+python lenet-11.py
+python 1cov.py 0.25
+python 2process.py 0.25
+python 3predict.py 0.25
+python 4last-output.py 0.25
+python hsg-withcov.py 0.25
+# Add more lines for additional scripts as needed
+
+
+
+
+
+
+
+
+
+
+
